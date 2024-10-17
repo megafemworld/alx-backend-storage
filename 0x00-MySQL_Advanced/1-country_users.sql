f10ae5c09f32d3c48e895e19f4904ac2f4e8378b
Femi Mehalayese <megafemclick@gmail.com>
2024-10-17 10:29:18 +0100

    using constraint check

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
 -- Create table user with country
-DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'IN') NOT NULL DEFAULT 'US' 
+	country VARCHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN')) 
 );
