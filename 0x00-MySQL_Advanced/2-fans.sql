fa03f5b0ca990e6f922217bef66f95b8b5a5ee8f
Femi Mehalayese <megafemclick@gmail.com>
2024-10-17 12:40:58 +0100

    fans base sum

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- SQL S=script thst  rank by country
 -- order by number
-SELECT origin, SUM(fans) AS nb_fans
-	FROM mental_bands GROUP BY origin
-	ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS mb_fans
+	FROM mental_bands
+	GROUP BY origin
+	ORDER BY mb_fans DESC;
