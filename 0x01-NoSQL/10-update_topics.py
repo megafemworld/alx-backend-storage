08638712d376f060a87a3adfc726b9b9c3908d42
Femi Mehalayese <megafemclick@gmail.com>
2024-10-22 18:30:28 +0100

    update many

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"name": topics}}
+    )
