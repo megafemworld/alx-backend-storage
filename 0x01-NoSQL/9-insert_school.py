ccf14a1409d42b40e9a57d3c2a0d101a951dc199
Femi Mehalayese <megafemclick@gmail.com>
2024-10-22 17:12:16 +0100

    9 full

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,7 +2,11 @@
 """inserts a new document"""
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection with kwargs"""
+    """inserts a new document in a collection with kwargs
+    params:
+        mongo_collection: objects of the mongo collection
+        kwargs: key value pair of the data to add to the db
+    """
     d_insert = {}
     for key, value in kwargs.items():
         d_insert[key] = value
