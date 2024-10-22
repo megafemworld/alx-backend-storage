d7291129662175ee2653b786da1dcc17280ea745
Femi Mehalayese <megafemclick@gmail.com>
2024-10-22 17:09:56 +0100

    long journey

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """inserts a new document"""
 
-
 def insert_school(mongo_collection, **kwargs):
-    """ inserts a new document in a collection with kwargs"""
+    """inserts a new document in a collection with kwargs"""
     d_insert = {}
     for key, value in kwargs.items():
         d_insert[key] = value
     if len(d_insert) > 0:
-        mongo_collection.insert_one(d_insert)
+        adder = mongo_collection.insert_one(d_insert)
+        return adder.inserted_id
