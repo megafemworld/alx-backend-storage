eb19d33db3d8f712df73e0dd2f47c582c30e597d
Femi Mehalayese <megafemclick@gmail.com>
2024-10-24 22:46:52 +0100

    method mispelling

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,9 +19,11 @@ def call_history(method: Callable) -> Callable:
     """store the history of inputs and outputs for a particular function"""
     @wraps(method)
     def wrapper(self:Any, *args) -> str:
-        """wrap for the decorator"""
+        """wrap for the decorator to track
+            and argument
+        """
         self._redis.rpush(f'(method.__qualname__):inputs', str(args))
-        output = methdo(self, *args)
+        output = method(self, *args)
         self._redis.rpush(f'(method.__qualname__):outputs', output)
         return output
     return wrapper
